f7c71ffc9771efeb484ef580b9bbd610fac29609
antontoniavskyi <53255050+antontoniavskyi@users.noreply.github.com>
2023-08-21 18:58:53 +0300

    delete technical text

--- T1000_joystick.ino
+++ T1000_joystick.ino
@@ -1,11 +1,3 @@
-/*
-   This ESP32 code is created by esp32io.com
-
-   This ESP32 code is released in the public domain
-
-   For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-joystick
-*/
-
 #include <ezButton.h>
 
 #define VRX_PIN  36 // ESP32 pin GPIO36 (ADC0) connected to VRX pin
